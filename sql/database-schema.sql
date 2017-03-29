c808a61fbdc15c1227b2d282918b294cb10bab1a
Andra Simionov <andra.simionov@payu.ro>
2017-03-29 11:58:38 +0300

    Add query to create database and tables for online shop

---
+++ sql/database-schema.sql
@@ -0,0 +1,37 @@
+# table schema queries
+CREATE DATABASE IF NOT EXISTS `online_shop`;
+
+USE `online_shop`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+	`IdUser` INT(11) NOT NULL AUTO_INCREMENT,
+	`UserName` VARCHAR(100) NOT NULL DEFAULT '',
+	`Email` VARCHAR(100) NOT NULL DEFAULT '',
+	`Password` VARCHAR(40) NOT NULL DEFAULT '' COMMENT 'SHA-1 algorithm.',
+	PRIMARY KEY (`IdUser`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0;
+
+CREATE TABLE IF NOT EXISTS `products` (
+	`IdProduct` INT(11) NOT NULL AUTO_INCREMENT,
+	`ProductName` VARCHAR(50) NOT NULL DEFAULT '',
+	`Price` INT(5) NOT NULL,
+	PRIMARY KEY (`IdProduct`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0;
+
+CREATE TABLE IF NOT EXISTS `orders` (
+	`IdOrder` INT(11) NOT NULL AUTO_INCREMENT,
+	`IdUser` INT(11) NOT NULL,
+	`IdProduct` INT(11) NOT NULL,
+	`TotalAmount` INT(10) NOT NULL,
+	PRIMARY KEY (`IdOrder`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0;
+
