8a872c0c7a12f556b2573c42dc7f0310439923bb
diana.drajneanu <diana.drajneanu@payu.ro>
2017-04-01 19:11:13 +0300

    add missing parameters to http post request

--- web/application/controllers/Payment.php
+++ web/application/controllers/Payment.php
@@ -26,21 +26,24 @@ class Payment extends CI_Controller
         $this->PaymentModel->saveOrder($idUser, $idProduct);
 
         $apiCredentials = $this->AuthenticatorModel->getApiCredentials($email);
-        $this->sendOrder($apiCredentials, $productInfo->Price, $productInfo->Currency);
+        $this->sendOrder($apiCredentials, $email, $productInfo->Price, $productInfo->Currency);
     }
 
 
-   function sendOrder($apiCredentials, $price, $currency)
+   function sendOrder($apiCredentials, $email, $price, $currency)
    {
        $headers = [
-           'Authorization :' . $apiCredentials->ClientId . ';' . $apiCredentials->SecretKey,
+           'Authorization :' . $apiCredentials->ClientId . ',' . $apiCredentials->SecretKey,
            'Content-Type: application/json',
        ];
 
        $data = [
+           'id' => $this->generateUUID(),
+           'timestamp' => gmdate('Y-m-d h:i:s'),
+           'email' => $email,
            'orderData' => [
                'amount' => $price,
-               'currencly' => $currency
+               'currency' => $currency
            ]
        ];
 
@@ -58,4 +61,29 @@ class Payment extends CI_Controller
            echo $this->httpclient->getErrorMsg();
        }
    }
+
+    private function generateUUID()
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+
 }
