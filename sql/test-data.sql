d0d4b128629789d8f858710e67c25b7654db910a
diana.drajneanu <diana.drajneanu@payu.ro>
2017-04-01 15:51:29 +0300

    add one more user

--- sql/test-data.sql
+++ sql/test-data.sql
@@ -1,4 +1,5 @@
 insert into users (`Username`, `Email`, `Password`) values ('diana', 'diana@email.com', 12345);
+insert into users (`Username`, `Email`, `Password`) values ('andra', 'andra@email.com', 12345);
 
 insert into products (`ProductName`, `Price`) values ('product1', 12, 'RON');
 insert into products (`ProductName`, `Price`) values ('product2', 21, 'RON');
