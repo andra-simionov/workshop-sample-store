2d98476a3f58310c1041ba538d34aad9d5edbe1a
diana.drajneanu <diana.drajneanu@payu.ro>
2017-04-02 15:39:09 +0300

    add order reference and hashed password in test data

--- sql/test-data.sql
+++ sql/test-data.sql
@@ -1,7 +1,7 @@
 USE sample_store;
 
-insert into users (Username, Email, Password) values ('diana', 'diana@yahoo.com', 12345);
-insert into users (`Username`, `Email`, `Password`) values ('andra', 'andra@yahoo.com', 12345);
+insert into users (Username, Email, Password) values ('diana', 'diana@yahoo.com', 'FpzDtOh4YG.ea52fc619ec7905b56bfc9d978a3cf37');
+insert into users (`Username`, `Email`, `Password`) values ('andra', 'andra@yahoo.com', 'FpzDtOh4YG.ea52fc619ec7905b56bfc9d978a3cf37');
 
 insert into products (`ProductName`, `Price`, `Currency`) values ('product1', 12, 'RON');
 insert into products (`ProductName`, `Price`, `Currency`) values ('product2', 21, 'RON');
@@ -14,9 +14,9 @@ insert into products (`ProductName`, `Price`, `Currency`) values ('product8', 45
 insert into products (`ProductName`, `Price`, `Currency`) values ('product9', 13, 'RON');
 insert into products (`ProductName`, `Price`, `Currency`) values ('product10', 22, 'RON');
 
-insert into orders (`IdUser`, `IdProduct`, `Date`) values (1, 2, '2017-01-02 14:00:10');
-insert into orders (`IdUser`, `IdProduct`, `Date`) values (1, 1, '2017-02-03 18:07:00');
-insert into orders (`IdUser`, `IdProduct`, `Date`) values (1, 3, '2017-03-14 19:54:00');
+insert into orders (`IdUser`, `IdProduct`, `Date`, `OrderReference`) values (1, 2, '2017-01-02 14:00:10', '23454');
+insert into orders (`IdUser`, `IdProduct`, `Date`, `OrderReference`) values (1, 1, '2017-02-03 18:07:00', '14254');
+insert into orders (`IdUser`, `IdProduct`, `Date`, `OrderReference`) values (1, 3, '2017-03-14 19:54:00', '87453');
 
 INSERT INTO `user_credentials` (`ClientId`, `SecretKey`, `Email`, `AddDate`) VALUES ('Andra_ID19', 'Andra_TEST', 'andra@yahoo.com', NOW()),
   ('Diana_ID10', 'Diana_TEST', 'diana@yahoo.com', NOW());
