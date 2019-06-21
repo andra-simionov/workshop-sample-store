8595741a6e8b2c1220564018c9e65d27824660e1
Horatiu Brinza <horatiu.brinza@payu.ro>
2019-06-21 16:06:55 +0300

    added getApiCredentials()

--- web/application/models/AuthenticatorModel.php
+++ web/application/models/AuthenticatorModel.php
@@ -7,4 +7,21 @@ class AuthenticatorModel extends CI_Model
         parent::__construct();
     }
 
+	/**
+	 * @return stdClass
+	 *
+	 * @throws Exception
+	 */
+	public function getApiCredentials()
+	{
+		$result = $this->db->select('*')
+			->from('store_credentials')
+			->get();
+
+		if ($result->first_row() != NULL) {
+			return $result->first_row();
+		} else {
+			throw new \Exception("Can't provide api credentials!");
+		}
+	}
 }
