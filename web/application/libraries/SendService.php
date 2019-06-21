2e5ffb90cff4c4e737da7f28879e221062a75a41
andra.simionov@payu.ro <andra.simionov@payu.ro>
2019-06-22 00:15:35 +0300

    [workshop-sample-store] Revive refund

--- web/application/libraries/SendService.php
+++ web/application/libraries/SendService.php
@@ -119,4 +119,61 @@ class SendService
     {
         return $responseParameters = json_decode($response, true);
     }
+
+    /**
+     * @param string $token
+     * @param string $email
+     * @param int $price
+     * @param string $currency
+     * @param string $orderReference
+     *
+     * @return array
+     *
+     * @throws Exception
+     */
+    function refundOrder($token, $email, $price, $currency, $orderReference)
+    {
+        $headers = $this->CI->apiclient->getHeaders();
+
+        $data = [
+            'requestId' => $this->CI->apiclient->generateUUID(),
+            'timestamp' => date('Y-m-d h:i:s'),
+            'email' => $email,
+            'token' => $token,
+            'orderData' => [
+                'reference' => $orderReference,
+                'amount' => $price,
+                'currency' => $currency
+            ]
+        ];
+
+        $this->CI->load->library('HttpClient',
+            [
+                'headers' => $headers,
+                'data' => json_encode($data),
+                'url' => ApiClient::BANK_URL . ApiClient::API_ENDPOINT_REFUND
+            ]
+        );
+
+        if ($this->CI->httpclient->post()){
+            return $this->CI->httpclient->getResults();
+        } else {
+            throw new \Exception($this->CI->httpclient->getErrorMsg());
+        }
+    }
+
+    /**
+     * @param string $response
+     */
+    public function interpretRefundApiResponse($response)
+    {
+        $responseParameters = $this->parseApiResponse($response);
+        $orderReference = $responseParameters['orderData']['reference'];
+
+        if ($responseParameters['meta']['status'] == 'OK') {
+            $this->CI->OrderModel->updateOrderStatus($orderReference, Refund::ORDER_STATUS_REFUNDED);
+        } else {
+            $this->CI->OrderModel->updateOrderStatus($orderReference, Refund::ORDER_STATUS_REJECTED);
+        }
+    }
 }
